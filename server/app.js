a7164b6c25a7f804259459780ad91b423409084a
mablchnole <mablchnole@gmail.com>
2016-07-12 22:53:30 -0500

    post route is functional

--- server/app.js
+++ server/app.js
@@ -3,23 +3,25 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
-// middleware
-app.use(bodyParser.urlencoded({extended: true}));
+// set client directory as static
 app.use(express.static('client'));
+
+// middleware
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// include routes
+var addPlaymate = require('./routes/addPlaymate');
+
+// use routes
+app.use('/', addPlaymate);
+
 
 // base url to show path resolved index.html
 app.get('/', function(req, res){
   console.log('bow wow at basecamp');
   res.sendFile(path.resolve('client/views/index.html'));
 });
-
-// include routes
-var addplaymate = require('./routes/addplaymateroute');
-
-
-
-
-
 
 // set up server
 app.set('port', process.env.PORT || 3000);
--- server/routes/addplaymate.js
+++ server/routes/addplaymate.js
@@ -8,15 +8,15 @@ var pg = require('pg');
 var connection = require('../modules/connection');
 
 // post route to add new playmate
-router.post('/addPlaymate', function(req, res){
+router.post('/addPlaymate', function (req, res){
   console.log('in addPlaymate server post route', req.body.name);
-  pg.connect(connectionString, function(err, client, done){
+  pg.connect(connection, function(err, client, done){
     if(err){
       console.log(err);
     } else {
       var sendPlaymate = client.query('INSERT INTO playmates (name, breed, age, gender) VALUES ($1, $2, $3, $4)',
         [req.body.name, req.body.breed, req.body.age, req.body.gender]);
-      sendTask.on('end', function(){
+      sendPlaymate.on('end', function(){
         return res.end();
       });
     }
