911ecdd2123b8a69267594b89a7fd99c3d849260
mablchnole <mablchnole@gmail.com>
2016-07-15 10:05:50 -0500

    successfully accessing playstyles in array in object

--- client/scripts/controllers/ProfileController.js
+++ client/scripts/controllers/ProfileController.js
@@ -8,7 +8,6 @@ angular.module('myApp').controller('ProfileController', [
   function($scope, $rootScope, $http, $window, $location, Upload) {
     $rootScope.playmateProfile = [];
     $rootScope.playmateMatches = [];
-    console.log('trying to access playstyle array values:', $rootScope.playmateProfile);
 
     // retrieve newest playmate created from server
     $scope.displayProfile = function() {
@@ -18,6 +17,10 @@ angular.module('myApp').controller('ProfileController', [
       }).then(function(response) {
         $rootScope.playmateProfile = response.data;
         console.log('SUCCESS, newest playmate:', response.data);
+        console.log('trying to access first playstyle:', response.data[0].playstyles[0]);
+
+        // i know that i need to access the playstyle values in the response.data
+        // but i don't know how to access it
       }, function myError (response) {
         console.log(response.statusText);
       });
@@ -28,10 +31,8 @@ angular.module('myApp').controller('ProfileController', [
     $scope.displayMatches = function() {
       // collect user input to send to database
       var playstylesToSend = {
-        // playstyles: response.data.playstyles ? How do I access these values???
-        playstyles: 'Chaser' // hardcode to test route
+        playstyle1: response.data[0].breed
       };
-
       console.log('sending to server:', playstylesToSend);
       // post method to send input data to database
       $http({
