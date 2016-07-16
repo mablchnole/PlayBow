d277720efa14f9e6cad722eee224608e841fa7b1
mablchnole <mablchnole@gmail.com>
2016-07-16 11:16:27 -0500

    custom matches kind of works

--- client/scripts/controllers/ProfileController.js
+++ client/scripts/controllers/ProfileController.js
@@ -11,6 +11,7 @@ angular.module('myApp').controller('ProfileController', [
     $rootScope.playmateMatches = [];
 
 
+
     // get method to retrieve all playmates
     $scope.getPlaymatesToFilter = function() {
       // retrieve data from server
@@ -24,6 +25,7 @@ angular.module('myApp').controller('ProfileController', [
         console.log(response.statusText);
       });
     }; // end getPlaymatesToFilter
+
     $scope.getPlaymatesToFilter();
 
     // get method to retrieve newest playmate created from server
@@ -35,27 +37,21 @@ angular.module('myApp').controller('ProfileController', [
         $rootScope.playmateProfile = response.data;
         console.log('YES! Newest playmate:', $rootScope.playmateProfile);
         // find matches for this profile
-        for(var i=0; $rootScope.allPlaymates.length > i; i++) {
-          console.log('inside for loop, all playmates accessed:', $rootScope.allPlaymates);
-          console.log('inside for loop, accessing new playmate:', $rootScope.playmateProfile);
-          console.log('playmate profile playstyle', $rootScope.playmateProfile[0].playstyles[0]);
-          console.log('loop through all and access playstyle', $rootScope.allPlaymates[i].playstyles[0]);
-          // if this playmate's playstyle 1, 2 & 3 matches any in all playmate playstyles
-          // then push this playmate to an array to displayProfile
-          if($rootScope.playmateProfile[0].playstyles[0] === $rootScope.allPlaymates[i].playstyles[0] || $rootScope.playmateProfile[0].playstyles[1] === $rootScope.allPlaymates[i].playstyles[1] || $rootScope.playmateProfile[0].playstyles[2] === $rootScope.allPlaymates[i].playstyles[2]) {
+        for(var i=0; i < $rootScope.allPlaymates.length; i++) {
+          console.log('inside for loop, all plcess playstyle', $rootScope.allPlaymates[i].playstyles[0]);
+          if($rootScope.playmateProfile[0].playstyles[0] === $rootScope.allPlaymates[i].playstyles[0]) {
             $rootScope.playmateMatches.push($rootScope.allPlaymates[i]);
+            console.log('array of playmate matches', $rootScope.playmateMatches);
           } else {
-            return false;
+            console.log("We're sorry. There are no matches at this time!");
           }
-        }
+        } // end for loop
 
       }, function myError (response) {
         console.log(response.statusText);
       });
     }; // end displayProfile
     $scope.displayProfile();
-
-    // function to display all matches
 
 
 
